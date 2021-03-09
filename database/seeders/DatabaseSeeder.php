c8a5b1e33ec039dc0c5c01a8371aac1718d5406d
mbrabenec <brabzzz@gmail.com>
2021-03-09 22:11:11 +0100

    ul

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BookshopsSeeder::class);
-        $this->call(ReviewsSeeder::class);
+        // $this->call(BookshopsSeeder::class);
+        // $this->call(ReviewsSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
